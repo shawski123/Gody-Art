1c5aa4dfaa0c1487127c048f62247a51dda73ea9
shawski123 <sean2me2016@gmail.com>
2025-08-10 15:51:55 -0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -6,61 +6,111 @@
 #include <imgui-SFML.h>
 #include <imgui.h>
 
+#include <windows.h>
+#include <commdlg.h>
+
 enum {
-    width,
-    height
+	width,
+	height
 };
 
-int main() {
-    int windowSize[2] = { 600,600};
-    char fileName [256] = "drawing";
-    bool showWin = true;
+void SeparatorText(const char* label);
 
-    sf::RenderWindow window(sf::VideoMode(windowSize[width], windowSize[height]), "Gody Art");
-    window.setFramerateLimit(144);
-    ImGui::SFML::Init(window);
-    bool my_tool_active = true;
-    float my_color[4] = { .5f,.5f,.5f,.5f };
-    bool clear = false;
-    sf::Vector2f pix_size = { 10,10 };
-    float single_pix_size = 5;
-    std::vector<std::vector<sf::RectangleShape>> undo;
-    std::vector<sf::RectangleShape> temp;
-    std::vector<sf::RectangleShape> strokes;
-    sf::RectangleShape bg(sf::Vector2f(windowSize[width], windowSize[height]));
-    bg.setOrigin(bg.getLocalBounds().width / 2, bg.getLocalBounds().height / 2);
-    bg.setFillColor(sf::Color::White);
-    bg.setPosition(windowSize[width] / 2, windowSize[height] / 2);
+int main() {
+	bool isctrlopressed = true;
+	bool spriteOn = true;
+	int windowSize[2] = { 600,600 }; // 600,600 by default
+	int projectSize[2] = { 0,0 };
+	char fileName[256] = "";
+	bool showWin = true;
+	float zoomSpeed = 0.1f;
+
+	OPENFILENAME ofn;
+	char openFile[MAX_PATH] = "";
+
+	//Open file
+	ZeroMemory(&ofn, sizeof(ofn));
+	ofn.lStructSize = sizeof(ofn);
+	ofn.hwndOwner = NULL; // No specific owner window
+	ofn.lpstrFilter = "PNG Images\0*.png\0";
+	ofn.lpstrFile = openFile;
+	ofn.nMaxFile = MAX_PATH;
+	ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+
+	sf::Texture texture;
+
+	sf::RenderWindow window(sf::VideoMode(windowSize[width], windowSize[height]), "Gody Art");
+	window.setFramerateLimit(144);
+	ImGui::SFML::Init(window);
+	bool my_tool_active = true;
+	float my_color[4] = { .5f,.5f,.5f,.5f };
+	bool clear = false;
+	sf::Vector2f pix_size = { 10,10 };
+	float single_pix_size = 20;
+	std::vector<std::vector<sf::RectangleShape>> undo;
+	std::vector<sf::RectangleShape> temp;
+	std::vector<sf::RectangleShape> strokes;
+	sf::RectangleShape bg(sf::Vector2f(0, 0));
+	bg.setFillColor(sf::Color::White);
 
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
 
-    std::vector<sf::RectangleShape> eraseVec;
+	bool isctrlzpressed = false;
 
-    bool isctrlzpressed = false;
-    bool isctrlwpressed = false;
+	sf::Clock deltaClock;
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			ImGui::SFML::ProcessEvent(event);
 
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(event);
+			if (event.type == sf::Event::MouseWheelScrolled) {
+				if (event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel) {
+					float delta = event.mouseWheelScroll.delta;
+					float zoomFactor = 1.f + delta * zoomSpeed;
 
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        ImGui::SetNextWindowSize(ImVec2(600,600), ImGuiCond_Always);
-        ImGui::SetNextWindowPos(ImVec2(0,0));
-        if (showWin) {
-			ImGui::Begin("Project Settings");
-			ImGui::InputInt2("Window Size", windowSize);
-			if (ImGui::InputText("Save Location", fileName, 256)) {
-
+					if (zoomFactor > 0) {
+						sf::View view = window.getView();
+						view.setCenter(800, 360);
+						view.zoom(1.f / zoomFactor);
+						window.setView(view);
+					}
+				}
 			}
-			if (ImGui::Button("Save and Close")) {
+
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::SetNextWindowSize(ImVec2(600, 600), ImGuiCond_Always);
+		ImGui::SetNextWindowPos(ImVec2(0, 0));
+		if (showWin) {
+			ImGui::Begin("Project Configuration");
+			SeparatorText("Project Size");
+			ImGui::InputInt2("", projectSize);
+			SeparatorText("Size Presets");
+			if (ImGui::Button("64x64")) {
+				projectSize[width] = 64;
+				projectSize[height] = 64;
+			}
+			if (ImGui::Button("128x128")) {
+				projectSize[width] = 128;
+				projectSize[height] = 128;
+			}
+			if (ImGui::Button("256x256")) {
+				projectSize[width] = 256;
+				projectSize[height] = 256;
+			}
+			SeparatorText("Save location");
+			if (ImGui::InputText("", fileName, 256)) {
+			}
+			if (ImGui::Button("Create")) {
+				//Set window size to 720p and center it
+				windowSize[width] = 1280;
+				windowSize[height] = 720;
+
 				window.setSize(sf::Vector2u(windowSize[width], windowSize[height]));
 
 				sf::View view = window.getView();
@@ -69,26 +119,40 @@ int main() {
 				window.setView(view);
 				bg.setSize(sf::Vector2f(windowSize[width], windowSize[height]));
 
-                window.setPosition(sf::Vector2i(
-                    (desktop.width - window.getSize().x) / 2,
-                    (desktop.height - window.getSize().y) / 2
-                ));
-                showWin = false;
+				window.setPosition(sf::Vector2i(
+					(desktop.width - window.getSize().x) / 2,
+					(desktop.height - window.getSize().y) / 2
+				));
+				//Set project size
+				bg.setSize(sf::Vector2f(projectSize[width], projectSize[height]));
+				bg.setOrigin(bg.getLocalBounds().width / 2, bg.getLocalBounds().height / 2);
+				bg.setPosition({ 800, 360 });
+				showWin = false;
 			}
-            ImGui::End();
-        }
+			ImGui::End();
+		}
 
-        ImGui::SetNextWindowSize(
-            ImVec2(float(windowSize[width] / 4), float(windowSize[height])), ImGuiCond_Always
-        );
-        ImGui::SetNextWindowPos(ImVec2(0, 0));
-        if (!showWin) {
+		ImGui::SetNextWindowSize(
+			ImVec2(float(windowSize[width] / 4), float(windowSize[height])), ImGuiCond_Always
+		);
+		ImGui::SetNextWindowPos(ImVec2(0, 0));
+		if (!showWin) {
 			ImGui::Begin("Pixel Editor", &my_tool_active, ImGuiWindowFlags_MenuBar);
 			if (ImGui::BeginMenuBar())
 			{
-				if (ImGui::BeginMenu("Project"))
+				if (ImGui::BeginMenu("Project Settings"))
 				{
-					if (ImGui::MenuItem("Open..", "Ctrl+O")) { /* Do stuff */ }
+					if (ImGui::MenuItem("Open..", "Ctrl+O")) {
+						spriteOn = true;
+						if (GetOpenFileName(&ofn)) {
+							std::cout << "Selected file: " << openFile << std::endl;
+						}
+						else {
+							std::cout << "No file selected or dialog canceled." << std::endl;
+						}
+
+						texture.loadFromFile(openFile);
+					}
 					if (ImGui::MenuItem("Save", "Ctrl+S")) {
 						sf::RenderTexture renderTexture;
 						renderTexture.create(windowSize[width], windowSize[height]);
@@ -117,119 +181,164 @@ int main() {
 				clear = true;
 			}
 
-			if (ImGui::InputFloat("Thickness", &single_pix_size)) {
+			if (ImGui::Button("Delete Image")) {
+				spriteOn = false;
+			}
+
+			if (ImGui::InputFloat("px", &single_pix_size)) {
 				pix_size = { single_pix_size, single_pix_size };
 			}
 
 			ImGui::End();
-        }
-
-        sf::Color color(
-            static_cast<sf::Uint8>(my_color[0] * 255),
-            static_cast<sf::Uint8>(my_color[1] * 255),
-            static_cast<sf::Uint8>(my_color[2] * 255),
-            static_cast<sf::Uint8>(my_color[3] * 255)
-        );
-
-        if (!ImGui::GetIO().WantCaptureMouse && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            sf::RectangleShape pixel(pix_size);
-            pixel.setOrigin({ pixel.getLocalBounds().width / 2, pixel.getLocalBounds().height / 2 });
-            pixel.setFillColor(color);
-            sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-
-            // Snap to grid
-			mousePos.x = ((mousePos.x + (int)pix_size.x / 2) / (int)pix_size.x) * (int)pix_size.x;
-			mousePos.y = ((mousePos.y + (int)pix_size.y / 2) / (int)pix_size.y) * (int)pix_size.y;
-
-            pixel.setPosition(sf::Vector2f(mousePos));
-            strokes.push_back(pixel);
-            temp.push_back(pixel);
-        }
-        else if (!temp.empty()) {
-            undo.push_back(temp);
-            temp.clear();
-        }
-
-        //Erase
-		if (!ImGui::GetIO().WantCaptureMouse && sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
-			sf::RectangleShape erase(pix_size);
-			erase.setFillColor(sf::Color::White);
-            erase.setOrigin({ erase.getLocalBounds().width / 2, erase.getLocalBounds().height / 2 });
-
-			sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-
-			// Snap to grid
-			mousePos.x = ((mousePos.x + (int)pix_size.x / 2) / (int)pix_size.x) * (int)pix_size.x;
-			mousePos.y = ((mousePos.y + (int)pix_size.y / 2) / (int)pix_size.y) * (int)pix_size.y;
-
-
-            erase.setPosition(sf::Vector2f(mousePos));
-            eraseVec.push_back(erase);
 		}
 
-        if (clear == true) {
-            strokes.clear();
-            undo.clear();
-            eraseVec.clear();
-            clear = false;
-        }
+		sf::Color color(
+			static_cast<sf::Uint8>(my_color[0] * 255),
+			static_cast<sf::Uint8>(my_color[1] * 255),
+			static_cast<sf::Uint8>(my_color[2] * 255),
+			static_cast<sf::Uint8>(my_color[3] * 255)
+		);
 
-        //Undo CTRL+Z
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z) && !isctrlzpressed) {
-            if (!temp.empty()) {
-                undo.push_back(temp);
-                temp.clear();
-            }
 
-            if (!undo.empty()) {
-                std::vector<sf::RectangleShape>& lastStroke = undo.back();
-                if (strokes.size() >= lastStroke.size()) {
-                    strokes.resize(strokes.size() - lastStroke.size());
-                    undo.pop_back();
-                }
-            }
-            isctrlzpressed = true;
-        }
-        if (!sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) || !sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
-            isctrlzpressed = false;
-        }
-        //Close CTRL+W
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            ImGui::SFML::Shutdown(); return 0;
-        }
-        //Save to file CTRL+S
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            sf::RenderTexture renderTexture;
-            renderTexture.create(windowSize[width], windowSize[height]);
-            renderTexture.clear(sf::Color::White);
-            
-            for (auto& pixel : strokes) {
-                renderTexture.draw(pixel);
-            }
-            renderTexture.display();
+		//Opened image
+		sf::Sprite sprite(texture);
+		sf::Vector2f spriteSize = { sprite.getLocalBounds().width, sprite.getLocalBounds().height };
+		sprite.setOrigin({ spriteSize.x / 2, spriteSize.y / 2 });
+		sprite.setPosition(800, 360);
 
-            sf::Image image = renderTexture.getTexture().copyToImage();
+		float scaleFactor = 1.0f;
+		if (spriteSize.x > projectSize[width] || spriteSize.y > projectSize[height]) {
+			float scaleX = projectSize[width] / spriteSize.x;
+			float scaleY = projectSize[height] / spriteSize.y;
+			scaleFactor = std::min(scaleX, scaleY);
+		}
+
+		sprite.scale(scaleFactor, scaleFactor);
+
+		//Draw
+		if (!ImGui::GetIO().WantCaptureMouse && sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+			sf::Vector2i mousePixelPos = sf::Mouse::getPosition(window);
+			sf::Vector2f mouseWorldPos = window.mapPixelToCoords(mousePixelPos);
+
+			// Snap to grid in world coords
+			mouseWorldPos.x = static_cast<int>(mouseWorldPos.x / pix_size.x) * pix_size.x + pix_size.x / 2.f;
+			mouseWorldPos.y = static_cast<int>(mouseWorldPos.y / pix_size.y) * pix_size.y + pix_size.y / 2.f;
+			if (bg.getGlobalBounds().contains(mouseWorldPos)) {
+				sf::RectangleShape pixel(pix_size);
+				pixel.setOrigin({ pixel.getLocalBounds().width / 2, pixel.getLocalBounds().height / 2 });
+				pixel.setFillColor(color);
+				pixel.setPosition(mouseWorldPos);
+				strokes.push_back(pixel);
+				temp.push_back(pixel);
+			}
+		}
+		else if (!temp.empty()) {
+			undo.push_back(temp);
+			temp.clear();
+		}
+
+		//Erase
+		if (!ImGui::GetIO().WantCaptureMouse && sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
+			sf::Vector2i mousePixelPos = sf::Mouse::getPosition(window);
+			sf::Vector2f mouseWorldPos = window.mapPixelToCoords(mousePixelPos);
+
+			// Snap to grid in world coords
+			mouseWorldPos.x = static_cast<int>(mouseWorldPos.x / pix_size.x) * pix_size.x + pix_size.x / 2.f;
+			mouseWorldPos.y = static_cast<int>(mouseWorldPos.y / pix_size.y) * pix_size.y + pix_size.y / 2.f;
+
+			for (auto& pixel : strokes) {
+				if (pixel.getPosition() == mouseWorldPos) {
+					pixel.setFillColor(sf::Color::White);
+				}
+			}
+		}
+
+		//Clears screen
+		if (clear == true) {
+			strokes.clear();
+			undo.clear();
+			spriteOn = false;
+			clear = false;
+		}
+
+		//Undo CTRL+Z
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z) && !isctrlzpressed) {
+			if (!temp.empty()) {
+				undo.push_back(temp);
+				temp.clear();
+			}
+
+			if (!undo.empty()) {
+				std::vector<sf::RectangleShape>& lastStroke = undo.back();
+				if (strokes.size() >= lastStroke.size()) {
+					strokes.resize(strokes.size() - lastStroke.size());
+					undo.pop_back();
+				}
+			}
+			isctrlzpressed = true;
+		}
+		if (!sf::Keyboard::isKeyPressed(sf::Keyboard::LControl) || !sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+			isctrlzpressed = false;
+		}
+		//Close CTRL+W
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+			ImGui::SFML::Shutdown(); return 0;
+		}
+		//Save to file CTRL+S
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+			sf::RenderTexture renderTexture;
+			renderTexture.create(projectSize[width], projectSize[height]);
+			renderTexture.clear(sf::Color::White);
+
+			for (auto& pixel : strokes) {
+				renderTexture.draw(pixel);
+			}
+			renderTexture.display();
+
+			sf::Image image = renderTexture.getTexture().copyToImage();
 			char fullFileName[260];
 			strcpy(fullFileName, fileName);
 			strcat(fullFileName, ".png");
 			image.saveToFile(fullFileName);
-            ImGui::SFML::Shutdown(); return 0;
-        }
+			ImGui::SFML::Shutdown(); return 0;
+		}
+		//Open file CTRL+O
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) && sf::Keyboard::isKeyPressed(sf::Keyboard::Key::O)) {
+			spriteOn = true;
+			if (GetOpenFileName(&ofn)) {
+				std::cout << "Selected file: " << openFile << std::endl;
+			}
+			else {
+				std::cout << "No file selected or dialog canceled." << std::endl;
+			}
 
-        window.clear();
-        window.draw(bg);
-        ImGui::SFML::Render(window);
-        for (auto& pixel : strokes) {
-            window.draw(pixel);
-        }
-        for (auto& pixel : eraseVec) {
-            window.draw(pixel);
-        }
-        window.display();
-    }
+			texture.loadFromFile(openFile);
+			isctrlopressed = true;
+		}
+		if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Key::LControl) || !sf::Keyboard::isKeyPressed(sf::Keyboard::Key::O)) {
+			isctrlopressed = false;
+		}
 
-    ImGui::SFML::Shutdown();
+		window.clear();
+		window.draw(bg);
+		if (spriteOn) {
+			window.draw(sprite);
+		}
+		ImGui::SFML::Render(window);
+		for (auto& pixel : strokes) {
+			window.draw(pixel);
+		}
+		window.display();
+	}
 
-    return 0;
+	ImGui::SFML::Shutdown();
+
+	return 0;
 }
 
+void SeparatorText(const char* label)
+{
+	ImGui::Separator();
+	ImGui::Text("%s", label);
+	ImGui::Separator();
+}
