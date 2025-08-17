d2c687d76f77de21790412569be730425d5ea190
shawski123 <sean2me2016@gmail.com>
2025-08-17 12:25:26 -0700

    Fixed saving

--- src/Util.cpp
+++ src/Util.cpp
@@ -6,17 +6,26 @@ void SeparatorText(const char* label) {
 	ImGui::Separator();
 }
 
-void saveFile(int projectSize[], std::vector<sf::RectangleShape>& strokes, char fileName[]) {
-	CreateDirectoryA("save", NULL); 
+void saveFile(int projectSize[], std::vector<sf::RectangleShape>& strokes, const char* fileName) {
+	// Get the folder containing the executable
+	char exePath[260];
+	GetModuleFileNameA(NULL, exePath, 260);
+	std::string exeFolder(exePath);
+	exeFolder = exeFolder.substr(0, exeFolder.find_last_of("\\/"));
 
+	// Ensure the "save" folder exists inside the exe folder
+	std::string saveFolder = exeFolder + "\\save";
+	CreateDirectoryA(saveFolder.c_str(), NULL);
+
+	// Setup render texture
 	sf::RenderTexture renderTexture;
 	renderTexture.create(projectSize[width], projectSize[height]);
 
 	sf::View view(sf::FloatRect(
 		640 - projectSize[width] / 2.f,
 		360 - projectSize[height] / 2.f,
-		projectSize[width],
-		projectSize[height]
+		static_cast<float>(projectSize[width]),
+		static_cast<float>(projectSize[height])
 	));
 	renderTexture.setView(view);
 
@@ -29,16 +38,16 @@ void saveFile(int projectSize[], std::vector<sf::RectangleShape>& strokes, char 
 	renderTexture.display();
 
 	sf::Image image = renderTexture.getTexture().copyToImage();
-	char path[260];
-	strcpy(path, "save/");
-	strcat(path, fileName);
-	strcat(path, ".png");
 
-	if (!image.saveToFile(path)) {
+	// Build full path
+	std::string fullPath = saveFolder + "\\" + fileName + ".png";
+
+	// Save to file
+	if (!image.saveToFile(fullPath)) {
 		MessageBoxA(NULL, "Failed to save the image!", "Error", MB_ICONERROR);
 	}
 	else {
-		std::string msg = std::string("Saved file to save/") + fileName + ".png";
+		std::string msg = "Saved file to " + fullPath;
 		MessageBoxA(NULL, msg.c_str(), "Success", MB_OK | MB_ICONASTERISK);
 	}
 }
--- src/Util.h
+++ src/Util.h
@@ -6,4 +6,4 @@
 #include "Editor.h"
 
 void SeparatorText(const char* label);
-void saveFile(int projectSize[], std::vector<sf::RectangleShape>& strokes, char fileName[]);
+void saveFile(int projectSize[], std::vector<sf::RectangleShape>& strokes, const char* fileName);
